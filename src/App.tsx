91def7e3218ad447deee0f6d16f4cc964e71a7ad
ToothyJarl <95950523+ToothyJarl@users.noreply.github.com>
2024-08-22 01:14:53 -0700

    feat: dropdown start

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ import Ripple from '@/components/magicui/ripple'
 
 import profile from '@/assets/images/toothyjarl.jpg'
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger,
+    DropdownMenuLabel,
+    DropdownMenuSeparator
+} from '@/components/ui/dropdown-menu'
+
 function App() {
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
@@ -14,27 +25,50 @@ function App() {
                         <div className="absolute inset-0 flex items-center justify-center">
                             <Ripple mainCircleSize={350} />
                         </div>
-                        <a
-                            href="https://github.com/ToothyJarl"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="relative w-60 h-60 flex items-center justify-center"
-                        >
-                            <img
-                                src={profile}
-                                alt="Profile"
-                                className="w-full h-full object-cover rounded-full transition-transform duration-500 hover:scale-95"
-                            />
-                        </a>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <a className="relative w-60 h-60 flex items-center justify-center cursor-pointer">
+                                    <img
+                                        src={profile}
+                                        alt="Profile"
+                                        className="w-full h-full object-cover rounded-full transition-transform duration-500 hover:scale-95"
+                                    />
+                                </a>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-56">
+                                <DropdownMenuLabel>aidanhgill@gmail.com</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuGroup>
+                                    <DropdownMenuItem>
+                                        <span>GitHub</span>
+                                        <DropdownMenuShortcut>github.com</DropdownMenuShortcut>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <span>Twitter</span>
+                                        <DropdownMenuShortcut>twitter.com</DropdownMenuShortcut>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <span>Discord</span>
+                                        <DropdownMenuShortcut>discord.com</DropdownMenuShortcut>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <span>Email</span>
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </div>
 
                     <div className="flex flex-col justify-center lg:w-1/3 w-full lg:pl-8 p-4 lg:p-8">
                         <div className="text-left text-white w-full">
                             <h1 className="text-3xl font-semibold">Hi there! I'm ToothyJarl,</h1>
                             <p className="mt-2 text-xl opacity-80">
-                                a young and aspiring software developer with a tendency to dabble in
-                                frontend development. My journey into programming began as a kid,
-                                and I’ve been passionate about it ever since.
+                                a young and aspiring software developer with a tendency to dabble
+                                with frontend development. My journey into programming began as a
+                                kid, and I’ve been passionate about it ever since.
+                            </p>
+                            <p className="mt-2 text-xl opacity-40">
+                                --- Click on my profile to view links and socials.
                             </p>
                         </div>
                     </div>
